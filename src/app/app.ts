aa6448bb454fbc88f01f7cfa086c84f2cd881cb0
Jonathan Macedo Castro <jonathan.castro18@outlook.com>
2025-11-03 17:24:50 -0300

    adicionando a tag do navbar.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,15 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Navbar } from './components/navbar/navbar';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar],
+  imports: [RouterOutlet, Navbar, CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('gerador-certificado');
+  public readonly exibeNavBar: boolean = true;
 }
