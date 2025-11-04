7a8857ad2b33731c9bdbab134154f6fe390acd23
Jonathan Macedo Castro <jonathan.castro18@outlook.com>
2025-11-04 09:59:13 -0300

    adicionando o componente de item do projeto.

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,13 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Navbar } from './components/navbar/navbar';
 import { CommonModule } from '@angular/common';
+import { PrimaryButton } from './components/primary-button/primary-button';
+import { SecondButton } from './components/second-button/second-button';
+import { ItemCertificado } from './components/item-certificado/item-certificado';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar, CommonModule],
+  imports: [RouterOutlet, Navbar, CommonModule, PrimaryButton, SecondButton, ItemCertificado],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
---
+++ src/app/components/item-certificado/item-certificado.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { SecondButton } from '../second-button/second-button';
+
+@Component({
+  selector: 'app-item-certificado',
+  imports: [SecondButton],
+  templateUrl: './item-certificado.html',
+  styleUrl: './item-certificado.css',
+})
+export class ItemCertificado {}
