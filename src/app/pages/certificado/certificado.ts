04bdcc3e882c3a52a07ee592053c32efc8af53c1
Jonathan Macedo Castro <jonathan.castro18@outlook.com>
2025-11-07 12:39:20 -0300

    adicionando o componente de certificado.

---
+++ src/app/pages/certificado/certificado.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-certificado',
+  imports: [],
+  templateUrl: './certificado.html',
+  styleUrl: './certificado.css',
+})
+export class Certificado {
+
+}
